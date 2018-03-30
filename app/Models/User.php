2ee517133b4dff00d24627520f233f5290a92dae
NonRn <rn.non@hotmail.com>
2018-03-30 14:31:55 +0700

    [Add] User Model

---
+++ app/Models/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Jenssegers\Mongodb\Eloquent\Model ;
+
+class User extends Model
+{
+    protected $collection = "User";
+    protected $connection = 'mongodb';
+}
